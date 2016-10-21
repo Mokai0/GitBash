f477778105d23f1cde393c4a6c650f926c358878
Mohammad Hanbali <mokai86@gmail.com>
2016-10-20 23:38:48 -0400

    some work

--- September2016/jquery/2spoilerComplete/js/moapp.js
+++
@@ -1,8 +0,0 @@
-//Look up that treevid off youtube about pokemon being ppl
-//make an icon that will serve as the video link
-//make the spiler info that pokemon are ppl
-
-
-
-//Hide the spoiler info
-//Reveal the spoiler info through user interaction
