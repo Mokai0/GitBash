2d3a9a807a0d6f58839049db17e755bc9b1c23e0
Mokai0 <mokai86@gmail.com>
2016-11-09 22:21:55 -0500

    finished w/npm

--- September2016/NodejsBasics/npmProjects/app.js
+++ September2016/NodejsBasics/npmProjects/app.js
@@ -4,7 +4,9 @@ var colors = require('colors');
 var bcrypt = require('bcryptjs');
 var salt = bcrypt.genSaltSync(10);
 var hash = bcrypt.hashSync(unsecurePlainTextPassword, salt);
-  console.log(hash.blue);
+console.log(hash.blue);
+console.log(hash.red);
+console.log(hash.green);
 
 
 // // Load hash from your password DB.
