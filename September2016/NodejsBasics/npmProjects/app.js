a37533a633879a59e9727da33ebb7634b8d9eade
Mokai0 <mokai86@gmail.com>
2016-11-09 21:51:43 -0500

    learning about npm dependencies and npm init

---
+++ September2016/NodejsBasics/npmProjects/app.js
@@ -0,0 +1,31 @@
+var unsecurePlainTextPassword = "password";
+
+var colors = require('colors');
+var bcrypt = require('bcryptjs');
+var salt = bcrypt.genSaltSync(10);
+var hash = bcrypt.hashSync(unsecurePlainTextPassword, salt);
+  console.log(hash.blue);
+
+
+// // Load hash from your password DB.
+// bcrypt.compareSync("B4c0/\/", hash); // true
+// bcrypt.compareSync("not_bacon", hash); // false
+// ```
+//
+// Auto-gen a salt and hash:
+//
+// ```javascript
+// var hash = bcrypt.hashSync('bacon', 8);
+// ```
+//
+// Usage - Async
+// -------------
+// To hash a password:
+//
+// ```javascript
+// var bcrypt = require('bcryptjs');
+// bcrypt.genSalt(10, function(err, salt) {
+//     bcrypt.hash("B4c0/\/", salt, function(err, hash) {
+//         // Store hash in your password DB.
+//     });
+// });
