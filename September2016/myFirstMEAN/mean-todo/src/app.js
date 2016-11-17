c6ffa01f308282c94d21fda53c59243e27bbb653
Mokai0 <mokai86@gmail.com>
2016-11-17 17:49:53 -0500

    examining existing AngularJS

--- September2016/myFirstMEAN/mean-todo/src/app.js
+++ September2016/myFirstMEAN/mean-todo/src/app.js
@@ -3,9 +3,12 @@
 var express = require('express');
 
 var app = express();
-
+var router = require('./api');
 app.use('/', express.static('public'));
 
+
+
+app.use('/api', router);
 
 app.listen(3000, function() {
   console.log('running on server localhost:3000')
