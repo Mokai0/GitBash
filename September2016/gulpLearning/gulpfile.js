4f0f5e4cd3ef3649485f2dd6a00b42be05d4962c
Mokai0 <mokai86@gmail.com>
2016-11-16 19:12:16 -0500

    finished gulp

--- September2016/gulpLearning/gulpfile.js
+++ September2016/gulpLearning/gulpfile.js
@@ -1,11 +1,58 @@
 'use strict';
 
-var gulp = require('gulp');
+var   gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var   sass = require('gulp-sass');
+var   maps = require('gulp-sourcemaps');
+var    del = require('del');
 
-gulp.task("hello", function() {
-  console.log("Hello!");
+gulp.task("concatScripts", function() {
+  console.log("Concatinating Scripts!");
+  return gulp.src(['js/jquery.js',
+            'js/sticky/jquery.sticky.js',
+            'js/main.js'])
+    .pipe(maps.init())
+    .pipe(concat("app.js"))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest("js"));
 });
 
-gulp.task("default", ["hello"], function() {
-  console.log("running the default task");
+gulp.task('minifyScripts', ["concatScripts"], function() {
+  console.log('Minifying the code!')
+  return gulp.src('js/app.js')
+    .pipe(uglify())
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task('compileSass', function() {
+  console.log("Making pages look nice!");
+  return gulp.src('scss/application.scss')
+    .pipe(maps.init())
+    .pipe(sass())
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('watchFiles', function() {
+  gulp.watch('scss/**/*.scss', ['compileSass']);
+  gulp.watch('js/main.js', ['concatScripts']);
+});
+
+gulp.task('clean', function() {
+  del(['dist', 'css/application.css*', 'js/app.*.js']);
+});
+
+gulp.task("build", ["minifyScripts", "compileSass"], function() {
+  return gulp.src(["css/application.css", "js/app.min.js", "index.html", "img/**", "fonts/**"], {base: './'})
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('serve', ['watchFiles']);
+
+gulp.task("default",['clean'], function() {
+  console.log("Running the tasks!");
+  gulp.start('build');
 });
