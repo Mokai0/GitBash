28221d20a9a6aaf3c83e922e1315265b0c17d23c
Mokai0 <mokai86@gmail.com>
2016-11-17 20:35:44 -0500

    there's a light at the end of the tunnel

--- September2016/myFirstMEAN/mean-todo/src/app.js
+++ September2016/myFirstMEAN/mean-todo/src/app.js
@@ -1,12 +1,16 @@
 'use strict';
 
+debugger;
+
 var express = require('express');
 
-var app = express();
 var router = require('./api');
+
+var app = express();
+
+require('./database');
+
 app.use('/', express.static('public'));
-
-
 
 app.use('/api', router);
 
